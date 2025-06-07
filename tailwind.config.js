01ab433d4eccb2fb83f7cfa87316c3fe4321b456
Daniel Murphy <danielmurph8@gmail.com>
2025-06-07 17:37:53 -0400

    cleanup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: [
+      './_includes/**/*.html',
+      './_layouts/**/*.html',
+      './_posts/*.md',
+      './*.html',
+    ],
+    darkMode: 'media',
+    theme: {
+      extend: {},
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
+  }
